d1dfe377da2b5f9b0a863eab461e5d874654454e
hpagcaliwagan <hpagcaliwagan@aeoncredit.com.ph>
2016-08-04 17:40:47 +0800

    add account pojo

---
+++ src/main/java/com/sleipnir/module/account/Account.java
@@ -0,0 +1,35 @@
+package com.sleipnir.module.account;
+
+
+public class Account {
+	private int id;
+	private String username;
+	private String password;
+	public Account(int id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
